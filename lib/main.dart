691fef36bd5a11fa789b22c01d245c52cca48a85
saminyeaser <saminyeaser1@gmail.com>
2020-05-30 18:33:37 +0600

    code redundency utiliazation

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,15 @@ class _XylophoneState extends State<Xylophone> {
     final player = AudioCache();
     player.play('assets/note$soundNumber.wav');
   }
+  void buildKey(){
+    FlatButton(
+      onPressed: (){
+        playSound(7);
+      },
+      child: Text('Click Me'),
+      color: Colors.red,
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,55 +37,7 @@ class _XylophoneState extends State<Xylophone> {
         ),
         body: Column(
           children: <Widget>[
-            FlatButton(
-              onPressed: (){
-                playSound(1);
-              },
-              child: Text('Click Me'),
-              color: Colors.deepPurple,
-            ),
-            FlatButton(
-              onPressed: (){
-                playSound(2);
-              },
-              child: Text('Click Me'),
-              color: Colors.blue,
-            ),
-            FlatButton(
-              onPressed: (){
-                playSound(3);
-              },
-              child: Text('Click Me'),
-              color: Colors.lightBlueAccent,
-            ),
-            FlatButton(
-              onPressed: (){
-               playSound(4);
-              },
-              child: Text('Click Me'),
-              color: Colors.green,
-            ),
-            FlatButton(
-              onPressed: (){
-                playSound(5);
-              },
-              child: Text('Click Me'),
-              color: Colors.yellow,
-            ),
-            FlatButton(
-              onPressed: (){
-                playSound(6);
-              },
-              child: Text('Click Me'),
-              color: Colors.orange,
-            ),
-            FlatButton(
-              onPressed: (){
-                playSound(7);
-              },
-              child: Text('Click Me'),
-              color: Colors.red,
-            )
+
           ],
         ),
       ),
